fdfb1ab02bb1623f531955de757ee2a1d8181f94
Andis Spriņķis <mail@andis.work>
2021-01-20 23:33:46 +0200

    TOOD

--- setup/setup.sh
+++ setup/setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
+# brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 brew update
 
+# command line programs
 brew install \
   wget \
   brew \
@@ -26,7 +28,13 @@ brew install \
   atool \
   tldr
 
+# iterm2
 brew install --cask iterm2
+## TODO: store and load iterm2 config
+
+
+# use zsh
+chsh -s /usr/bin/zsh
 
 wget --no-cache -P $HOME/ https://raw.githubusercontent.com/andis-sprinkis/linux-user-config/master/.zshrc
 if [ -f $HOME/.zshrc.1 ]
@@ -35,6 +43,14 @@ then
   mv .zshrc.1 .zshrc
 fi
 
+. $HOME/.zshrc
+
+
+# nvm
+## TODO: set and config nvm (node version)
+
+
+# neovim config
 mkdir -p $HOME/.config
 
 git clone git@github.com:andis-sprinkis/neovim-user-config.git $HOME/.config/nvim
